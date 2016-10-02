fdfda161cd8a46e74bd0e3ece8d835b0665e79f1
Steven Wong <sidiw1112@gmail.com>
2016-10-01 21:45:37 -0700

    add scripts: Camera Follow Player, scrolling and looping background, basic moving script by arrow key

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z--;
+			this.transform.position = position;
+		}
+	}
+}
