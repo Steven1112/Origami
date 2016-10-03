723edc213fe9e1beff364cbd6d474ac05c92c534
lukelee365 <li_lu@cdm-mbp-c10-wec.gnwc.private>
2016-10-02 20:08:37 -0700

    Fix Something and Add Something
    Notices and fixed:
    
    1 .Using 3D Rigid instead of 2D
    2. Assigned layer to Each game object for camera
    
    Need to Fixed(Steven go fixed it):
    
    1. reduce the lerp time of the camera to stop shaking camera
    
    Need to discuss:
    Infinite Map( Map recycle)
    Added:
    Add multi-layer mid ground and background
    add flip side of character
    add the mid ground movement
    add jump

---
+++ Assets/Scripts/MidGroundMoving.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MidGroundMoving : MonoBehaviour {
+	public float movingSpeedPlayer; //Moving speed accroding to player 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x = position.x - movingSpeedPlayer;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x = position.x + movingSpeedPlayer;
+			this.transform.position = position;
+		}
+	
+	}
+}
--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -2,32 +2,71 @@
 using System.Collections;
 
 public class MoveScript : MonoBehaviour {
-
+	public float movingSpeed;
+	private bool isLeft;
+	private bool isRight;
+	private bool isGround;
+	private  Rigidbody rb;
+	public float JumpForce;
+	void Awake(){
+		isLeft = false;
+		isRight = true;
+	}
+	void Start(){
+		rb = gameObject.GetComponent<Rigidbody>();
+	}
 	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow))
 		{
+			if (isRight)
+			{
+				//turan around
+				isLeft = true;
+				isRight = false;
+				transform.Rotate (Vector3.up*180f);
+			}
 			Vector3 position = this.transform.position;
-			position.x--;
+			position.x = position.x - movingSpeed;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.RightArrow))
+		if (Input.GetKey(KeyCode.RightArrow))
 		{
+			//turn
+			if (isLeft) 
+			{
+				isRight = true;
+				isLeft = false;
+				transform.Rotate (Vector3.up*-180f);
+			}
 			Vector3 position = this.transform.position;
-			position.x++;
+			position.x = position.x + movingSpeed;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			Vector3 position = this.transform.position;
-			position.z++;
+			position.z=position.z+movingSpeed;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+		if (Input.GetKey(KeyCode.DownArrow))
 		{
 			Vector3 position = this.transform.position;
-			position.z--;
+			position.z=position.z-movingSpeed;
 			this.transform.position = position;
 		}
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			if (isGround) 
+			{
+				Debug.Log (isGround);
+				rb.AddForce (Vector3.up * JumpForce);
+				isGround = false;
+			}
+		}
+	}
+	void OnCollisionEnter(Collision coll) {
+		if (coll.gameObject.tag == "Ground")
+			isGround = true;
 	}
 }
