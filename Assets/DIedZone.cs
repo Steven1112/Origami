b14c6225c0b0894dfe4a146cf0099fa014de4d93
lukelee365 <li_lu@cdm-mbp-c11-30.gnwc.private>
2016-10-26 22:51:44 -0700

    Fixed the Bridge

---
+++ Assets/DIedZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DIedZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Monster1") {
+			Destroy (collision.gameObject);
+		}
+	}
+}
---
+++ Assets/TurnOnGravity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOnGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Monster1") {
+			Rigidbody rigid =other.gameObject.GetComponent<Rigidbody> ();
+			rigid.useGravity = true;
+		}
+	}
+}
