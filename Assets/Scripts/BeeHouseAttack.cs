1a9df413d26d7784f95e1f61edf379c80b51137b
Steven Wong <sidiw1112@gmail.com>
2016-10-26 22:52:58 -0700

    update the fire, animations, add bees attack script

---
+++ Assets/Scripts/BeeHouseAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeHouseAttack : MonoBehaviour {
+	public float beesDmgPlayerPerSecond;
+	public float beesDmgMonsterPerSecond;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		InvokeRepeating ("OnCollisionEnter", 3f, 1f);
+
+	}
+
+	public void OnCollisionEnter(Collision coll){
+
+		print (coll);
+		if (coll.gameObject.tag=="Player") {
+			coll.gameObject.SendMessage ("TakeDmg", beesDmgPlayerPerSecond);
+			//Destroy (gameObject);
+		}
+		if (coll.gameObject.tag=="Monster1") {
+			//monster step on trap will destory it 
+			coll.gameObject.SendMessage ("TakeDmg", beesDmgMonsterPerSecond);
+			Destroy (gameObject);
+		}
+	}
+}
